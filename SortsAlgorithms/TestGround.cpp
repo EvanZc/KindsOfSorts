196927a69d84adf67df281b315aeff4e4d103179
EvanZeng <ZengCheng263@gmail.com>
2016-04-24 21:26:31 +0800

    Create the Project

---
+++ SortsAlgorithms/TestGround.cpp
@@ -0,0 +1,20 @@
+//
+//  TestGround.cpp
+//  SortsAlgorithms
+//
+//  Created by 曾诚 on 16-4-24.
+//
+//
+
+#include <iostream>
+
+#include "TestGround.h"
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World"<<endl;
+
+    return 0;
+}
---
+++ SortsAlgorithms/TestGround.h
@@ -0,0 +1,14 @@
+//
+//  TestGround.h
+//  SortsAlgorithms
+//
+//  Created by 曾诚 on 16-4-24.
+//
+//
+
+#ifndef __SortsAlgorithms__TestGround__
+#define __SortsAlgorithms__TestGround__
+
+#include <stdio.h>
+
+#endif /* defined(__SortsAlgorithms__TestGround__) */
