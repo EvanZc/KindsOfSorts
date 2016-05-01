60b89cb84cbf25cda1b50b60023b9b7846975908
EvanZeng <ZengCheng263@gmail.com>
2016-05-01 16:58:53 +0800

    Insert Sort
    Test is ok

--- SortAlgorithms/BasicSort.cpp
+++ SortAlgorithms/BasicSort.cpp
@@ -24,5 +24,16 @@ void BubbleSort(std::vector<int>& sort_vec)
 
 void InsertSort(std::vector<int>& sort_vec)
 {
-
+    long long j;
+    for (unsigned long i = 1; i < sort_vec.size(); i++) {
+        for (j = i - 1 ; j >= 0; j--) {
+            if (sort_vec[j] <= sort_vec[i]) {
+                break;
+            }
+        }
+        for (unsigned long start_move = i; start_move > j; start_move--) {
+            sort_vec[start_move] = sort_vec[start_move - 1];
+        }
+        sort_vec[j + 1] = sort_vec[i];
+    }
 }
--- SortAlgorithms/main.cpp
+++ SortAlgorithms/main.cpp
@@ -10,17 +10,39 @@
 #include <vector>
 #include "FileOperation.h"
 #include "ToolFunctions.h"
-#include "QuickSort.h"
+#include "Sort.h"
 #include "TestSortResult.h"
 
 int main(int argc, const char * argv[]) {
-    std::vector<std::vector<int> > vec = GetFixLenVecIntVecWithValueInSpecRange(1, 1000000, 0, 10000000);
     
-    QuickSortStandard(vec[0]);
+    int loop_time = 1000;
     
-    if (!IsVecIntAscendingOrder(vec[0])) {
-        std::cout<<"oops"<<std::endl;
+    while (loop_time--) {
+        std::vector<std::vector<int> > vec = GetFixLenVecIntVecWithValueInSpecRange(1, 1000, 0, 10000000);
+        
+        QuickSortStandard(vec[0]);
+        
+        if (!IsVecIntAscendingOrder(vec[0])) {
+            std::cout<<"oops quick sort"<<std::endl;
+        }
+        
+        std::vector<int> vec_bubble(vec[0]);
+        std::vector<int> vec_insert(vec[0]);
+        
+        BubbleSort(vec_bubble);
+        
+        if (!IsVecIntAscendingOrder(vec_bubble)) {
+            std::cout<<"oops bubble sort"<<std::endl;
+        }
+        
+        InsertSort(vec_insert);
+        
+        if (!IsVecIntAscendingOrder(vec_insert)) {
+            std::cout<<"oops insert sort"<<std::endl;
+        }
     }
+    
+    std::cout<<"yes ok!"<<std::endl;
     
     return 0;
 }
